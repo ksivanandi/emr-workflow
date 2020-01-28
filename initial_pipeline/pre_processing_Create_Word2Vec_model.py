5e8f51ba46ae46c6c115e75a925da5c52c4084f7
amalinow19821973 <59899841+amalinow19821973@users.noreply.github.com>
2020-01-28 12:28:44 -0500

    updated word2vec to read source data from db
    no longer reads list of tokens- also uses gensim's pre-processing library to prep for model creation.  Still need to play around with model creation and train parameters

--- initial_pipeline/pre_processing_Create_Word2Vec_model.py
+++ initial_pipeline/pre_processing_Create_Word2Vec_model.py
@@ -13,61 +13,88 @@ Author:  Andrew Malinow, PhD
 """
 
 
-# In[2]:
+# In[1]:
 
 
 """
 Imports
 """
+import requests
 import json
 import gensim
 from gensim.models import Word2Vec
+
+
+# In[2]:
+
+
+"""
+Global Variables
+"""
+resp = requests.get('http://10.32.22.16:56733/noteevents/55500')
+if resp.status_code != 200:
+        raise ApiError('GET /noteevents/<size> {}'.format(resp.status_code))
+json_notes=resp.json()['json_notes']
+notes_text = [note['text'] for note in json_notes]
+
+
+# In[3]:
+
+
+"""
+pre-processing for Word2Vec
+"""
+documents=[]
+for line in notes_text:
+    documents.append(gensim.utils.simple_preprocess (line))
 
 
 # In[4]:
 
 
 """
-Global Variables
-"""
-with open('tokens.json') as f:
-    data = json.load(f)
-
-
-# In[ ]:
-
-
-"""
 create word2vec model
 need to tune parameters
 """
-model = Word2Vec(data, min_count=2,workers=4)
+model = Word2Vec(documents, min_count=2,workers=10)
+model.train(documents,total_examples=len(documents),epochs=10)
 word_vectors=model.wv
 vocabulary=model.wv.vocab  
-sepsis_sim_words=model.wv.most_similar("sepsis")
-readmit_sim_words=model.wv.most_similar("readmission")
 
 
-# In[ ]:
+# In[7]:
 
 
 """
 save model, and word vectors
 """
 model.save('word2vec.model')
-model.save_word2vec_format('word2vecKeyVectors.model',binary=True)
+model.wv.save_word2vec_format('word2vecKeyVectors.model',binary=True)
 
 
-# In[ ]:
+# In[6]:
+
+
+sepsis_sim_words=model.wv.most_similar("infection")
+readmit_sim_words=model.wv.most_similar("readmitted")
+
+
+# In[18]:
 
 
 """
 write out similar terms ["sepsis" and "readmission"] to separate files
 these terms will be turned into features- e.g., sepsis_term_1 (0/1/); sepsis_term_2 (0/1)
 """
-with open ('sepsis_related_terms.json','wb') as f:
-    json.dumps(sepsis_sim_words,f)
+with open('sepsis_related.json','w') as f:
+    json.dump(dict(sepsis_sim_words), f)
 
-with open ('readmission_related_terms.json','wb') as f:
-    json.dumps(readmit_sim_words,f)
+with open('readmit_related.json','w') as a:
+    json.dump(dict(readmit_sim_words),a)
+
+
+# In[ ]:
+
+
+
 
