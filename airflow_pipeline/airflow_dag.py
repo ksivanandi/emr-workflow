a8092c3d36ffab8c089ad6e87d9f2c08b388ea82
morgworth <mworthington@5CG8392RV8.eplus.corp>
2020-01-28 14:00:08 -0800

    added all of the feature_engineering scripts as operators in the airflow pipeline

--- airflow_pipeline/airflow_dag.py
+++ airflow_pipeline/airflow_dag.py
@@ -2,6 +2,9 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from pre_processing_Create_Tokens import tokenize_data 
 from pre_processing_Create_Word2Vec_model import create_and_save_word2vec
+from feature_engineering_Sequence_Clinical_Note import add_admit_discharge_columns
+from feature_engineering_Topic_Modeling import run_topic_modeling
+from feature_engineering_Vitals_Related_Added import add_vitals_ngrams_columns
 from datetime import datetime, timedelta
 
 default_args = {
@@ -12,15 +15,33 @@ default_args = {
 dag = DAG('emr-initial-dag', default_args=default_args)
 
 tokenize_operator = PythonOperator(
-    task_id = 'create_tokens',
+    task_id = 'pre_processing_create_tokens',
     python_callable = tokenize_data,
     dag = dag
     )
 
 word_embedding_operator = PythonOperator(
-    task_id = 'create_word2vec_model',
+    task_id = 'pre_processing_create_word2vec_model',
     python_callable = create_and_save_word2vec,
     dag = dag
     )
 
-tokenize_operator >> word_embedding_operator
+admit_discharge_features_operator = PythonOperator(
+    task_id = 'feature_engineering_admit_discharge',
+    python_callable = add_admit_discharge_columns,
+    dag = dag
+    )
+
+topic_modeling_operator = PythonOperator(
+    task_id = 'feature_engineering_topic_modeling',
+    python_callable = run_topic_modeling,
+    dag = dag
+    )
+
+vitals_ngrams_features_operator = PythonOperator(
+    task_id = 'feature_engineering_vitals_ngrams',
+    python_callable = add_vitals_ngrams_columns,
+    dag = dag
+    )
+
+tokenize_operator >> [word_embedding_operator, admit_discharge_features_operator, topic_modeling_operator] >> vitals_ngrams_features_operator
