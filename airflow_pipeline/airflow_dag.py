e0e6e54693de631ffa0d3a6b599f52466ecd3ecf
emrm1 <morgan.worthington@eplus.com>
2020-02-10 17:35:42 +0000

    removed old operators and connected new operators

--- airflow_pipeline/airflow_dag.py
+++ airflow_pipeline/airflow_dag.py
@@ -1,11 +1,5 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-import pre_processing_Create_Word2Vec_model
-import feature_engineering_Sequence_Clinical_Note
-import feature_engineering_Topic_Modeling
-import feature_engineering_Vitals_Related_Added
-import tpot_prep_One_Hot_Encoding_Diagnoses
-import tpot_prep_One_Hot_Encoding_Medications
 
 import first_table_from_api
 import cleanse_notes
@@ -66,41 +60,4 @@ infected_one_hot_operator = PythonOperator(
     dag = dag
     )
 
-word_embedding_operator = PythonOperator(
-    task_id = 'pre_processing_create_word2vec_model',
-    python_callable = pre_processing_Create_Word2Vec_model.create_and_save_word2vec,
-    dag = dag
-    )
-
-
-admit_discharge_features_operator = PythonOperator(
-    task_id = 'feature_engineering_admit_discharge',
-    python_callable = feature_engineering_Sequence_Clinical_Note.add_admit_discharge_columns,
-    dag = dag
-    )
-
-topic_modeling_operator = PythonOperator(
-    task_id = 'feature_engineering_topic_modeling',
-    python_callable = feature_engineering_Topic_Modeling.run_topic_modeling,
-    dag = dag
-    )
-
-vitals_ngrams_features_operator = PythonOperator(
-    task_id = 'feature_engineering_vitals_ngrams',
-    python_callable = feature_engineering_Vitals_Related_Added.add_vitals_ngrams_columns,
-    dag = dag
-    )
-
-diagnoses_one_hot_operator = PythonOperator(
-    task_id = 'tpot_prep_diagnoses_one_hot',
-    python_callable = tpot_prep_One_Hot_Encoding_Diagnoses.diagnoses_one_hot_encoding,
-    dag = dag
-    )
-
-medications_one_hot_operator = PythonOperator(
-    task_id = 'tpot_prep_medications_one_hot',
-    python_callable = tpot_prep_One_Hot_Encoding_Medications.medications_one_hot_encoding,
-    dag = dag
-    )
-
-df_from_api_operator >> clean_notes_operator >> tokenize_notes_operator >> [word_embedding_operator, admit_discharge_features_operator, topic_modeling_operator] >> vitals_ngrams_features_operator >> diagnoses_one_hot_operator >> medications_one_hot_operator
+df_from_api_operator >> clean_notes_operator >> tokenize_notes_operator >> word2vec_operator >> entity_recognition
