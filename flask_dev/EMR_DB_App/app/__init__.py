bf1e2900ad4d213f4e731d13877522c4a7ad3b1b
emrm1 <morgan.worthington@eplus.com>
2020-04-03 00:30:14 +0000

    changed ip address for connecting to the db

--- flask_dev/EMR_DB_App/app/__init__.py
+++ flask_dev/EMR_DB_App/app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://emrm1:emrm1@10.32.23.1:5432/mimic?options=-csearch_path=mimiciii,public'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://emrm1:emrm1@10.32.22.6:5432/mimic?options=-csearch_path=mimiciii,public'
 db = SQLAlchemy(app)
 from app import views
