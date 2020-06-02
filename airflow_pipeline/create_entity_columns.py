f996600e6333fb009ccb9f4caf44df24aa9da590
Morgan Worthington <mworthington@eplus-dgx.cluster.local>
2020-06-02 16:36:11 -0700

    added logic to extract covid entities

--- airflow_pipeline/create_entity_columns.py
+++ airflow_pipeline/create_entity_columns.py
@@ -5,6 +5,7 @@ def extract_entities(note):
     lines = note.split('\n')
     medications = []
     features = []
+    covid_terms = []
 
     for line in lines:
         words = line.split()
@@ -36,30 +37,44 @@ def extract_entities(note):
                         else:
                             feature += '_' + sub_ent.split('[')[0]
                     features.append(feature)
+                elif 'B-COVID' in words[word_index]:
+                    while ent_end < len(words) and 'I-COVID' in words[ent_end]:
+                        ent_end += 1
+                    covid_term = possible_negation
+                    for i, sub_ent in enumerate(words[word_index:ent_end]):
+                        if i == 0:
+                            covid_term += sub_ent.split('[')[0]
+                        else:
+                            covid_term += '_' + sub_ent.split('[')[0]
+                    covid_terms.append(covid_term)
                 word_index += ent_end
             else:
                 word_index += 1
 
-    return medications, features
+    return medications, features, covid_terms
 
 
 def get_columns_from_notes(df):
 
     all_medication_entities = []
     all_general_feature_entities = []
+    all_covid_term_entities = []
 
     for i, row in df.iterrows():
         note = row['labeled_notes']
-        medications, features = extract_entities(note)
+        medications, features, covid_terms = extract_entities(note)
         #remove duplicates from the list:
         medications = list(dict.fromkeys(medications))
         features = list(dict.fromkeys(features))
+        covid_terms = list(dict.fromkeys(covid_terms))
         #
         all_medication_entities.append(medications)
         all_general_feature_entities.append(features)
+        all_covid_term_entities.append(covid_terms)
 
     df['medication_entities'] = all_medication_entities
     df['feature_entities'] = all_general_feature_entities
+    df['covid19_entities'] = all_covid_term_entities
 
     return df
         
