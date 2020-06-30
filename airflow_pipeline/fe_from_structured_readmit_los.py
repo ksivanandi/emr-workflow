663ffc8d8d14e9b350460094b78feff3a06a3097
ubuntu <morgan.worthington@eplus.com>
2020-06-30 14:04:20 -0700

    added age column to structured features script

--- airflow_pipeline/fe_from_structured_readmit_los.py
+++ airflow_pipeline/fe_from_structured_readmit_los.py
@@ -5,34 +5,45 @@ import numpy as np
 import datetime
 from workflow_read_and_write import standard_read_from_db, standard_write_to_db
 
-def add_los_and_binary_deathtime_columns(df):
-    los_list=[]
-    dt_binary_list = []
-    for row in df.iterrows():
-        admit = pd.to_datetime(row[1]['admittime'])
-        discharge = pd.to_datetime(row[1]['dischtime'])
+def add_los_age_and_binary_deathtime_columns(df):
+    los_list = []
+    age_list = []
+    for i, row in df.iterrows():
+        admit = pd.to_datetime(row['admittime'])
+        discharge = pd.to_datetime(row['dischtime'])
+
         los_timedelta = discharge - admit
         los_days_int = los_timedelta.days
         los_list.append(los_days_int)
+
+        dob = pd.to_datetime(row['dob'])
+        age = float(admit.year - dob.year)
+        #patients older than 89 had their age shifted 300 years to follow HIPAA in MIMIC, the median age for this group is 91.4
+        if age > 150:
+            age = 91.4
+        age_list.append(age)
+
     df['los'] = los_list
     df['death_time_present'] = df['deathtime'].notnull()
+    df['age'] = age_list
+
     return df
 
 def add_readmission_column(df):
     readmit_list = []
     readmit_threshold = pd.to_timedelta('30 days 00:00:00')
     zero_timedelta = pd.to_timedelta('0 days 00:00:00')
-    for row in df.iterrows():
-        current_admittime = pd.to_datetime(row[1]['admittime'])
+    for i, row in df.iterrows():
+        current_admittime = pd.to_datetime(row['admittime'])
         
-        patient_id = row[1]['patient_id']
+        patient_id = row['patient_id']
         same_patient_df = df.loc[df['patient_id'] == patient_id]
 
         readmit = False
-        for subrow in same_patient_df.iterrows():
+        for i, subrow in same_patient_df.iterrows():
             #don't compare the row to itself
-            if subrow[1]['admission_id'] != row[1]['admission_id']:
-                sub_dischtime = pd.to_datetime(subrow[1]['dischtime'])
+            if subrow['admission_id'] != row['admission_id']:
+                sub_dischtime = pd.to_datetime(subrow['dischtime'])
                 time_between_visits = current_admittime - sub_dischtime
                 #first conditional statement filters out future subrow visits from the current row
                 if time_between_visits > zero_timedelta and time_between_visits <= readmit_threshold:
@@ -46,7 +57,7 @@ def create_structured_data_features():
     df_json = df_json_encoded.decode()
     df = pd.read_json(df_json)
 
-    df = add_los_and_binary_deathtime_columns(df)
+    df = add_los_age_and_binary_deathtime_columns(df)
     df = add_readmission_column(df)
     
     df_json = df.to_json()
