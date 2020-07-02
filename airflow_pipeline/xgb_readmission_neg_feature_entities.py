1055e33a552f137e34859c75fe3591bd0075d9fe
ubuntu <morgan.worthington@eplus.com>
2020-07-01 18:41:50 -0700

    reduced top features from 5 to 3 for all xgboost models

--- airflow_pipeline/xgb_readmission_neg_feature_entities.py
+++ airflow_pipeline/xgb_readmission_neg_feature_entities.py
@@ -58,7 +58,7 @@ def make_predictions():
     
     df = add_predictions_column(df, bst, feat_one_hot)
 
-    top_n_df = make_top_n_features(bst, feat_one_hot, 5)
+    top_n_df = make_top_n_features(bst, feat_one_hot, 3)
 
     df_json_encoded = df.to_json().encode()
     top_n_df_json_encoded = df.to_json().encode()
